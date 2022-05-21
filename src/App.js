6efbe16f931e935d32eeddc36f50871313f39d31
a-sha0234 <asharma_31@hotmail.com>
2022-05-21 23:26:29 +0100

    starting files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    
+  </div>;
 }
 
 export default App;
---
+++ src/components/card.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+
+export default function Card(props) {
+  console.log(props);
+  return <main>a</main>;
+}
