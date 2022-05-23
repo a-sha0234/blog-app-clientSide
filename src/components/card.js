9992bc5edbaf74bc1bfc0a22bed87329a4fc689e
a-sha0234 <asharma_31@hotmail.com>
2022-05-23 14:39:52 +0100

    update card component

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,19 @@
 import { useState, useEffect } from "react";
 
 export default function Card(props) {
-  console.log(props);
-  return <main>a</main>;
+  let timeCreated = props.post.createdAt;
+  let strTimeCreated = timeCreated.split("T"); // store date and time created
+
+  let route = `/post/${props.post._id}`;
+
+  return (
+    <main>
+      <a href={route}>
+        <div>
+          <p>{props.post.title}</p>
+        </div>
+        <div>Date posted: {strTimeCreated[0]}</div>
+      </a>
+    </main>
+  );
 }
