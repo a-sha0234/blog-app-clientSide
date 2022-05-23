ef110559fc11b89ba162e301bc739d16d305f99f
a-sha0234 <asharma_31@hotmail.com>
2022-05-23 14:41:17 +0100

    fetch data from endpoint anc create dynamic routes for each blog

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Card from "./components/Card";
+import BlogDetails from "./components/BlogDetails";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  const [postsData, setPostsData] = useState([]);
+
+  const [isDataLoading, setIsDataLoading] = useState(); //
+
+  useEffect(() => {
+    fetch("http://localhost:3002/allposts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((result) => {
+        return result.json();
+      })
+      .then((posts) => {
+        setPostsData(posts);
+      });
+  }, []);
+
+  useEffect(() => {
+    // check if api request has been fufilled
+    if (postsData.length > 0) {
+      setIsDataLoading(false);
+    } else {
+      setIsDataLoading(true);
+    }
+  }, [postsData]);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        {" "}
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home apiData={postsData} />}></Route>
+        {/* Dynamically create routes for each post */}
+        {isDataLoading == false &&
+          postsData.map((post) => {
+            let route = `/post/${post._id}`;
+            return (
+              // <Route path={route} element={<Card apiData={post} />}></Route>
+              <Route
+                path={route}
+                element={<BlogDetails apiData={post} />}
+              ></Route>
+            );
+          })}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
