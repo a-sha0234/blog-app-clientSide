3fb813c9ddf5c33ad69b2544b0e8d4dede6c646c
a-sha0234 <asharma_31@hotmail.com>
2022-05-24 16:06:05 +0100

    display details about blog

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,3 +1,21 @@
+import CommentForm from "./CommentForm";
+
 export default function BlogDetails(props) {
-  return <div>{props.apiData.title}</div>;
+  console.log(props);
+  return (
+    <div className="blogDetails">
+      <div className="blogDetails__top">
+        <h1>{props.apiData.title}</h1>
+        <h2>{props.apiData.Author}</h2>
+        <p>{props.apiData.createdAt}</p>
+        <p>{props.apiData.updatedAt}</p>
+      </div>
+      <section className="blogDetails__body">{props.apiData.blogtext}</section>
+
+      <section>
+        {/* send blog data as  */}
+        <CommentForm data={props} />
+      </section>
+    </div>
+  );
 }
