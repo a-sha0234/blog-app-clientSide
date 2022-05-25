9191e1d8d33bbadd7c82ba837ff6e7f078d35fc8
a-sha0234 <asharma_31@hotmail.com>
2022-05-25 22:46:47 +0100

    make the whole card a link to the route

--- src/components/card.js
+++ src/components/card.js
@@ -7,18 +7,25 @@ export default function Card(props) {
   let route = `/post/${props.post._id}`; // route for each dteails about about blog
 
   return (
-    <main>
-      <a href={route}>
-        <div>
-          <p>{props.post.title}</p>
-        </div>
-        <div>
-          <p>Date posted: {strTimeCreated[0]}</p>
-        </div>
-        <div>
-          <p>Last updated: {strTimeCreated[0]}</p>
-        </div>
-      </a>
-    </main>
+    <a href={route} className="container">
+      <main className="cardContainer">
+        <div className="cardContainer__side"></div>
+        <section className="cardContainer__content">
+          {/* <div> */}
+          <p className="cardContainer__title">{props.post.title}</p>
+          {/* </div> */}
+          {/* <div> */}
+          <p className="cardContainer__date">
+            Date posted: {strTimeCreated[0]}
+          </p>
+          {/* </div> */}
+          {/* <div> */}
+          <p className="cardContainer__date">
+            Last updated: {strTimeCreated[0]}
+          </p>
+          {/* </div> */}
+        </section>
+      </main>
+    </a>
   );
 }
